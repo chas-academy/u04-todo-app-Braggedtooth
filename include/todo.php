d8cba48a6d8ac547b8f6963c21a6a294e98811b5
Adebayo Ajayi <lakeside1996@live.com>
2021-01-28 19:49:27 +0100

    delete completed task after 2 days & fixed alert bugs

--- include/todo.php
+++ include/todo.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once 'db_connect.php';
 session_start();
 
@@ -12,44 +12,40 @@ global $mysqli;
 
     
    
-        if(isset($_POST['submit'])){
+        if (isset($_POST['submit'])) {
             $title = $_POST['todo_item'];
             $description = $_POST['todo_desc'];
            
-            if ($title !=""){
+            if (empty($title)) {
+                $_SESSION['message'] = "Input Invalid ";
+            } else {
                 $mysqli->query("INSERT INTO `user`( `todo_item`,`todo_desc`, `date`)VALUES ('$title','$description', CURRENT_TIME())")or die($mysqli->error) ;
                 $_SESSION['message'] = "Task Added ";
                 $_SESSION['msg_type'] ="is-success";
-            }else {
-                $_SESSION['message'] = "Input Invalid ";
-                header("location:index.php");
             }
-    
         };
     
 
 
     
 
-    if(isset($_GET['delete'])){
+    if (isset($_GET['delete'])) {
         $id = $_GET['delete'];
         $mysqli->query("DELETE FROM `user` WHERE id = $id") or die($mysqli->error) ;
         $_SESSION['message'] ="Task Deleted!";
         $_SESSION['msg_type'] ="is-danger";
-        
     }
-    if( isset($_GET['edit'])){
+    if (isset($_GET['edit'])) {
         $id = $_GET['edit'];
         $check = $mysqli->query("SELECT* FROM `user` WHERE id = $id") or die($mysqli->error);
-            $row = $check->fetch_array();
-            $title = $row['todo_item'];
-            $description = $row['todo_desc'];
-            $_SESSION['message'] ="Edit Task!";
-            $_SESSION['msg_type'] ="is-link";
+        $row = $check->fetch_array();
+        $title = $row['todo_item'];
+        $description = $row['todo_desc'];
+        $_SESSION['message'] ="Edit Task!";
+        $_SESSION['msg_type'] ="is-link";
             
        
-            //Hämtar datan med id och lägg i en ny formulär så att användaren kan se och sedan kunna ändra. 
-       
+        //Hämtar datan med id och lägg i en ny formulär så att användaren kan se och sedan kunna ändra.
     }
 
     if (isset($_POST['update'])) {
@@ -60,7 +56,7 @@ global $mysqli;
             $mysqli->query("UPDATE `user` SET `todo_item` = '$title' , `todo_desc`='$description'  WHERE id=$id ") or die($mysqli->error);
             $_SESSION['message'] ="Todo Updated!";
             $_SESSION['msg_type'] ="is-warning";
-        }else {
+        } else {
             $_SESSION['message'] ="Press EDIT ";
             $_SESSION['msg_type'] ="is-warning";
         }
@@ -68,39 +64,40 @@ global $mysqli;
     //check if task is done
     $done= 0 ;
     
-    if(isset($_GET['complete'])){
+    if (isset($_GET['complete'])) {
         global $done;
         global $id;
-        $id = $_GET['complete']; 
+        $id = $_GET['complete'];
         $complete = $mysqli->query("SELECT `complete` FROM `user` WHERE id = $id") or die($mysqli->error);
         $row = $complete->fetch_assoc();
         $done = (int)$row['complete'] ;
-          if ($done == 0){
-              $done = 1;
-              $_SESSION['message'] ="Task Completed!";
-              $_SESSION['msg_type'] ="is-success";
-              
-            } else { 
+        if ($done == 0) {
+            $done = 1;
+            $_SESSION['message'] ="Task Completed!";
+            $_SESSION['msg_type'] ="is-success";
+        } else {
             $done = 0;
             $_SESSION['message'] ="Task Incomplete!";
-              $_SESSION['msg_type'] ="is-info";
-           
-
+            $_SESSION['msg_type'] ="is-info";
+        }
+        if ($done==1) {
+            $mysqli->query("DELETE FROM `user` WHERE date() < (CURDATE()- INTERVAL  2 DAY)");
             
-        };  
-       /*  if ($task_complete == false|| $done== 0){
-            $complete_class = 'is-warning';
-            $complete_btn = 'Complete Task';
-          
-        } else{
-            $complete_class = 'is-success';
-            $complete_btn = 'Task Complete';
-        };  */ 
+        };
+        /*  if ($task_complete == false|| $done== 0){
+             $complete_class = 'is-warning';
+             $complete_btn = 'Complete Task';
 
-            $mysqli -> query("UPDATE `user` SET `complete`= '$done' WHERE id=$id ") or die($mysqli->error);
-        }; 
+         } else{
+             $complete_class = 'is-success';
+             $complete_btn = 'Task Complete';
+         };  */
+
+        $mysqli -> query("UPDATE `user` SET `complete`= '$done' WHERE id=$id ") or die($mysqli->error);
+    };
         
-//complete class and text changes 
+    
+//complete class and text changes
 
 
 
@@ -108,7 +105,7 @@ global $mysqli;
         /*     if (isset($_GET['complete'])==$id||$done==1){
                 $complete_class = 'is-success';
                 $complete_btn = 'Task Complete';
-               
+
 
             }else {
                  $complete_class = 'is-warning';
@@ -119,18 +116,17 @@ global $mysqli;
             global $id;
             if($done == 0){
                 echo `<a href="index.php?complete=<?php echo $id;?>" class="button is-warning">Complete Task</a>`;
-                 }else{ echo `<a href="index.php?complete=<?php echo $id;?>" class="button is-success">Task Completed </a>`;}; */
-                      
-          
-      
+}else{ echo `<a href="index.php?complete=<?php echo $id;?>" class="button is-success">Task Completed </a>`;}; */
 
 
-      
-    //fecth_array returnerar en string därav inability att ändrar complete status med funtionen ovan.
-        
-       
-       
 
-    
-    session_destroy();
- ?>
+
+
+
+//fecth_array returnerar en string därav inability att ändrar complete status med funtionen ovan.
+
+
+
+
+
+session_destroy();
